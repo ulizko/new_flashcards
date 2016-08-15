e36bf8c1ba9b02f653866146acc606cd6e5037ba
shurikovich <shurikovich@bigmir.net>
2016-08-15 08:54:22 +0000

    Add Search views

--- app/controllers/dashboard/search_controller.rb
+++ app/controllers/dashboard/search_controller.rb
@@ -1,4 +1,12 @@
-class SearchController < ApplicationController
-  def new
+module Dashboard
+  class SearchController < ApplicationController
+    def new
+      query = params[:search][:query].gsub(" ", ",")
+      @photos = Flickr.photos(query)
+      respond_to do |format|
+        format.html
+        format.js
+      end
+    end
   end
 end
--- app/services/flickr.rb
+++ app/services/flickr.rb
@@ -2,7 +2,7 @@ class Flickr
   FlickRaw.api_key = ENV['FLICKRAW_API_KEY']
   FlickRaw.shared_secret = ENV['FLICKRAW_SHARED_SECRET']
   
-  def photos(query)
+  def self.photos(query)
     ids = flickr.photos.search(tags: query).first(10).map(&:id)   
     info = ids.map { |id| flickr.photos.getInfo(:photo_id => id) }
     info.map { |i| FlickRaw.url(i) }
