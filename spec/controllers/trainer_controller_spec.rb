c8445db0392fb0bc5b7c8b51ed09ffa4856bc8b7
shurikovich <shurikovich@bigmir.net>
2016-09-16 15:07:27 +0000

    Refactor trainer controller spec

--- spec/controllers/trainer_controller_spec.rb
+++ spec/controllers/trainer_controller_spec.rb
@@ -1,255 +1,44 @@
 require 'rails_helper'
 require 'support/helpers/trainer_helper.rb'
+
 include TrainerHelper
+
 module Dashboard
   RSpec.describe TrainerController, type: :controller do
+
     describe 'review_card' do
-      describe 'correct translation' do
-        let!(:user) { create(:user) }
-        let!(:block) { create(:block, user: user) }
-        before(:each) { @controller.send(:auto_login, user) }
+      let!(:user) { create(:user) }
+      let!(:block) { create(:block, user: user) }
+      let!(:card) { create(:card, user: user, block: block) }
+      before(:each) { @controller.send(:auto_login, user) }
 
-        it 'repeat=1 quality=5' do
-          card = create(:card, user: user, block: block,
-                        interval: 1, repeat: 1, efactor: 2.5, quality: 5)
-          card = check_review_card(card, 'house', 1)
-          expect(I18n.l(card.review_date)).to eq(I18n.l(Time.zone.now + 1.days))
-          expect(card.interval).to eq(6)
-          expect(card.repeat).to eq(2)
-          expect(card.attempt).to eq(1)
-        end
+      context 'when correct translation' do
+        let(:right) { I18n.t(:correct_translation_notice) }
 
-        it 'repeat=1 quality=5' do
-          card = create(:card, user: user, block: block,
-                        interval: 1, repeat: 1, efactor: 2.5, quality: 5)
-          card = check_review_card(card, 'house', 1)
-          expect(card.efactor).to eq(2.6)
-          expect(card.quality).to eq(5)
-        end
-
-        it 'repeat=1 quality=4' do
-          card = create(:card, user: user, block: block,
-                        interval: 1, repeat: 1, efactor: 2.5, quality: 5)
-          card = check_review_card(card, 'RoR', 1)
-          card = check_review_card(card, 'house', 1)
-          expect(card.efactor).to eq(2.18)
-          expect(card.quality).to eq(4)
-        end
-
-        it 'repeat=1 quality=3' do
-          card = create(:card, user: user, block: block,
-                        interval: 1, repeat: 1, efactor: 2.5, quality: 5)
-          card = check_review_card(card, 'RoR', 2)
-          card = check_review_card(card, 'house', 1)
-          expect(card.efactor).to eq(1.5)
-          expect(card.quality).to eq(3)
-        end
-
-        it 'repeat=1 quality=3' do
-          card = create(:card, user: user, block: block,
-                        interval: 1, repeat: 1, efactor: 2.5, quality: 5)
-          card = check_review_card(card, 'RoR', 3)
-          card = check_review_card(card, 'house', 1)
-          expect(card.efactor).to eq(1.3)
-          expect(card.quality).to eq(3)
-        end
-
-        it 'repeat=2 quality=5' do
-          card = create(:card, user: user, block: block,
-                        interval: 6, repeat: 2, efactor: 2.6, quality: 5)
-          card = check_review_card(card, 'house', 1)
-          expect(I18n.l(card.review_date)).to eq(I18n.l(Time.zone.now + 6.days))
-          expect(card.interval).to eq(16)
-          expect(card.repeat).to eq(3)
-          expect(card.attempt).to eq(1)
-        end
-
-        it 'repeat=2 quality=5' do
-          card = create(:card, user: user, block: block,
-                        interval: 6, repeat: 2, efactor: 2.6, quality: 5)
-          card = check_review_card(card, 'house', 1)
-          expect(card.efactor).to eq(2.7)
-          expect(card.quality).to eq(5)
-        end
-
-        it 'repeat=2 quality=4' do
-          card = create(:card, user: user, block: block,
-                        interval: 6, repeat: 2, efactor: 2.6, quality: 5)
-          card = check_review_card(card, 'RoR', 1)
-          card = check_review_card(card, 'house', 1)
-          expect(card.efactor).to eq(2.28)
-          expect(card.quality).to eq(4)
-        end
-
-        it 'repeat=2 quality=3' do
-          card = create(:card, user: user, block: block,
-                        interval: 6, repeat: 2, efactor: 2.6, quality: 5)
-          card = check_review_card(card, 'RoR', 2)
-          card = check_review_card(card, 'house', 1)
-          expect(card.efactor).to eq(1.6)
-          expect(card.quality).to eq(3)
-        end
-
-        it 'repeat=2 quality=3' do
-          card = create(:card, user: user, block: block,
-                        interval: 6, repeat: 2, efactor: 2.6, quality: 5)
-          card = check_review_card(card, 'RoR', 3)
-          card = check_review_card(card, 'house', 1)
-          expect(card.efactor).to eq(1.3)
-          expect(card.quality).to eq(3)
-        end
-
-        it 'repeat=3 quality=5' do
-          card = create(:card, user: user, block: block,
-                        interval: 16, repeat: 3, efactor: 2.7, quality: 5)
-          card = check_review_card(card, 'house', 1)
-          expect(I18n.l(card.review_date)).to eq(I18n.l(Time.zone.now + 16.days))
-          expect(card.interval).to eq(45)
-          expect(card.repeat).to eq(4)
-          expect(card.attempt).to eq(1)
-        end
-
-        it 'repeat=3 quality=5' do
-          card = create(:card, user: user, block: block,
-                        interval: 16, repeat: 3, efactor: 2.7, quality: 5)
-          card = check_review_card(card, 'house', 1)
-          expect(card.efactor).to eq(2.8)
-          expect(card.quality).to eq(5)
-        end
-
-        it 'repeat=3 quality=4' do
-          card = create(:card, user: user, block: block,
-                        interval: 16, repeat: 3, efactor: 2.7, quality: 5)
-          card = check_review_card(card, 'RoR', 1)
-          card = check_review_card(card, 'house', 1)
-          expect(card.efactor).to eq(2.38)
-          expect(card.quality).to eq(4)
-        end
-
-        it 'repeat=3 quality=3' do
-          card = create(:card, user: user, block: block,
-                        interval: 16, repeat: 3, efactor: 2.7, quality: 5)
-          card = check_review_card(card, 'RoR', 2)
-          card = check_review_card(card, 'house', 1)
-          expect(card.efactor).to eq(1.7)
-          expect(card.quality).to eq(3)
-        end
-
-        it 'repeat=3 quality=3' do
-          card = create(:card, user: user, block: block,
-                        interval: 16, repeat: 3, efactor: 2.7, quality: 5)
-          card = check_review_card(card, 'RoR', 3)
-          card = check_review_card(card, 'house', 1)
-          expect(card.efactor).to eq(1.3)
-          expect(card.quality).to eq(3)
-        end
-
-        it 'repeat=1-3 quality=5' do
-          card = create(:card, user: user, block: block,
-                        interval: 1, repeat: 1, efactor: 2.5, quality: 5)
-          card = check_review_card(card, 'house', 1)
-          card.update(review_date: Time.zone.now)
-          card = check_review_card(card, 'house', 1)
-          card.update(review_date: Time.zone.now)
-          card = check_review_card(card, 'house', 1)
-          expect(I18n.l(card.review_date)).to eq(I18n.l(Time.zone.now + 16.days))
-          expect(card.interval).to eq(45)
-          expect(card.repeat).to eq(4)
-          expect(card.attempt).to eq(1)
-          expect(card.efactor).to eq(2.8)
-          expect(card.quality).to eq(5)
+        it "returned message about correct answer" do
+          allow(card).to receive(:check_translation).and_return(state: true, distance: 0)
+          put :review_card, params: { card_id: card.id, user_translation: card.translated_text }
+          expect(flash[:notice]).to eq(right)
         end
       end
 
-      describe 'incorrect translation' do
-        let!(:user) { create(:user) }
-        let!(:block) { create(:block, user: user) }
-        before(:each) { @controller.send(:auto_login, user) }
-
-        it 'repeat=1 attempt=1' do
-          card = create(:card, user: user, block: block,
-                        interval: 1, repeat: 1, efactor: 2.5, quality: 4)
-          card = check_review_card(card, 'RoR', 1)
-          expect(card.interval).to eq(1)
-          expect(card.repeat).to eq(1)
-          expect(card.attempt).to eq(2)
-          expect(card.efactor).to eq(2.18)
-          expect(card.quality).to eq(2)
+      context 'when did typo, but translation is correct' do
+        let(:typo) do I18n.t('translation_from_misprint_alert', user_translation: 'houce',
+                              original_text: card.original_text, translated_text: card.translated_text)
         end
-
-        it 'repeat=1 attempt=2' do
-          card = create(:card, user: user, block: block,
-                        interval: 1, repeat: 1, efactor: 2.5, quality: 4)
-          card = check_review_card(card, 'RoR', 2)
-          expect(card.interval).to eq(1)
-          expect(card.repeat).to eq(1)
-          expect(card.attempt).to eq(3)
-          expect(card.efactor).to eq(1.64)
-          expect(card.quality).to eq(1)
-        end
-
-        it 'repeat=1 attempt=3' do
-          card = create(:card, user: user, block: block,
-                        interval: 1, repeat: 1, efactor: 2.5, quality: 4)
-          card = check_review_card(card, 'RoR', 3)
-          expect(card.interval).to eq(1)
-          expect(card.repeat).to eq(1)
-          expect(card.attempt).to eq(4)
-          expect(card.efactor).to eq(1.3)
-          expect(card.quality).to eq(0)
+        it "returned message with typo" do
+          allow(card).to receive(:check_translation).and_return(state: true, distance: 1)
+          put :review_card, params: { card_id: card.id, user_translation: 'houce' }
+          expect(flash[:alert]).to eq(typo)
         end
       end
 
-      describe 'correct and incorrect translation' do
-        let!(:user) { create(:user) }
-        let!(:block) { create(:block, user: user) }
-        before(:each) { @controller.send(:auto_login, user) }
-
-        it 'repeat=1-3 quality=4' do
-          card = create(:card, user: user, block: block,
-                        interval: 1, repeat: 1, efactor: 2.5, quality: 4)
-          card = check_review_card(card, 'house', 1)
-          card.update(review_date: Time.zone.now)
-          card = check_review_card(card, 'house', 1)
-          card.update(review_date: Time.zone.now)
-          card = check_review_card(card, 'RoR', 1)
-          card = check_review_card(card, 'house', 1)
-          expect(I18n.l(card.review_date)).to eq(I18n.l(Time.zone.now + 1.days))
-          expect(card.interval).to eq(6)
-          expect(card.repeat).to eq(2)
-          expect(card.attempt).to eq(1)
-          expect(card.efactor).to eq(2.38)
-          expect(card.quality).to eq(4)
-        end
-
-        it 'repeat=1-3 quality=5' do
-          card = create(:card, user: user, block: block,
-                        interval: 1, repeat: 1, efactor: 2.5, quality: 4)
-          card = check_review_card(card, 'house', 1)
-          card.update(review_date: Time.zone.now)
-          card = check_review_card(card, 'RoR', 1)
-          card = check_review_card(card, 'house', 1)
-          card.update(review_date: Time.zone.now)
-          card = check_review_card(card, 'house', 1)
-          expect(I18n.l(card.review_date)).to eq(I18n.l(Time.zone.now + 6.days))
-          expect(card.interval).to eq(14)
-          expect(card.repeat).to eq(3)
-          expect(card.attempt).to eq(1)
-          expect(card.efactor).to eq(2.38)
-          expect(card.quality).to eq(5)
-        end
-
-        it 'repeat=3 attempt=4' do
-          card = create(:card, user: user, block: block,
-                        interval: 16, repeat: 3, efactor: 2.7, quality: 5)
-          card = check_review_card(card, 'RoR', 3)
-          card = check_review_card(card, 'house', 1)
-          expect(I18n.l(card.review_date)).to eq(I18n.l(Time.zone.now + 1.days))
-          expect(card.interval).to eq(6)
-          expect(card.repeat).to eq(2)
-          expect(card.attempt).to eq(1)
-          expect(card.efactor).to eq(1.3)
-          expect(card.quality).to eq(3)
+      context 'when translation is not correct' do
+        let(:wrong) { I18n.t(:incorrect_translation_alert) }
+        it "returned message about wrong answer" do
+          allow(card).to receive(:check_translation).and_return(state: false)
+          put :review_card, params: { card_id: card.id, user_translation: 'hauces' }
+          expect(flash[:alert]).to eq(wrong)
         end
       end
     end
