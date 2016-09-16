84cbea515dcd4ae1dc13338ff6e07f833e4d1520
shurikovich <shurikovich@bigmir.net>
2016-09-16 18:37:03 +0000

    Refactor card model spec

--- spec/controllers/trainer_controller_spec.rb
+++ spec/controllers/trainer_controller_spec.rb
@@ -1,7 +1,4 @@
 require 'rails_helper'
-require 'support/helpers/trainer_helper.rb'
-
-include TrainerHelper
 
 module Dashboard
   RSpec.describe TrainerController, type: :controller do
@@ -16,18 +13,17 @@ module Dashboard
         let(:right) { I18n.t(:correct_translation_notice) }
 
         it "returned message about correct answer" do
-          allow(card).to receive(:check_translation).and_return(state: true, distance: 0)
           put :review_card, params: { card_id: card.id, user_translation: card.translated_text }
           expect(flash[:notice]).to eq(right)
         end
       end
 
       context 'when did typo, but translation is correct' do
-        let(:typo) do I18n.t('translation_from_misprint_alert', user_translation: 'houce',
-                              original_text: card.original_text, translated_text: card.translated_text)
+        let(:typo) do 
+          I18n.t('translation_from_misprint_alert', user_translation: 'houce',
+                 original_text: card.original_text, translated_text: card.translated_text)
         end
         it "returned message with typo" do
-          allow(card).to receive(:check_translation).and_return(state: true, distance: 1)
           put :review_card, params: { card_id: card.id, user_translation: 'houce' }
           expect(flash[:alert]).to eq(typo)
         end
@@ -36,7 +32,6 @@ module Dashboard
       context 'when translation is not correct' do
         let(:wrong) { I18n.t(:incorrect_translation_alert) }
         it "returned message about wrong answer" do
-          allow(card).to receive(:check_translation).and_return(state: false)
           put :review_card, params: { card_id: card.id, user_translation: 'hauces' }
           expect(flash[:alert]).to eq(wrong)
         end
--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -2,181 +2,64 @@ require 'rails_helper'
 
 RSpec.describe Card, type: :model do
   let(:user) { create(:user) }
+  let(:block) { create(:block, user: user) }
+  let(:card) { create(:card, user: user, block: block) }
 
-  it 'create card with empty original text' do
-    card = Card.create(original_text: '', translated_text: 'house', user: user,
-                       block_id: 1)
-    expect(card.errors[:original_text]).to include('Необходимо заполнить поле.')
+  before do
+    travel_to Time.zone.now
   end
 
-  it 'create card with empty translated text' do
-    card = Card.create(original_text: 'дом', translated_text: '', user: user,
-                       block_id: 1)
-    expect(card.errors[:translated_text]).
-      to include('Необходимо заполнить поле.')
+  after { travel_back }
+
+  describe '#set_review_date_as_now' do
+    it 'setting review date' do
+      expect(card.review_date).to eq(Time.zone.now)
+    end
   end
 
-  it 'create card with empty texts' do
-    card = Card.create(original_text: '', translated_text: '', user: user,
-                       block_id: 1)
-    expect(card.errors[:original_text]).
-      to include('Вводимые значения должны отличаться.')
-  end
+  describe '#check_translation' do
+    context 'when right translation' do
+      context 'and after first review' do
+        before { card.check_translation('house') }
 
-  it 'equal_texts Eng' do
-    card = Card.create(original_text: 'house', translated_text: 'house',
-                       user: user, block_id: 1)
-    expect(card.errors[:original_text]).
-      to include('Вводимые значения должны отличаться.')
-  end
+        it { expect(card.review_date).to eq(Time.zone.now + 1.day) }
+        it { expect(card.quality).to eq(5) }
+      end
 
-  it 'equal_texts Rus' do
-    card = Card.create(original_text: 'дом', translated_text: 'дом', user: user,
-                       block_id: 1)
-    expect(card.errors[:original_text]).
-      to include('Вводимые значения должны отличаться.')
-  end
+      context 'and after twice review' do
+        before { 2.times { card.check_translation('house') } }
 
-  it 'full_downcase Eng' do
-    card = Card.create(original_text: 'hOuse', translated_text: 'houSe',
-                       user: user, block_id: 1)
-    expect(card.errors[:original_text]).
-      to include('Вводимые значения должны отличаться.')
-  end
+        it { expect(card.review_date).to eq(Time.zone.now + 6.days) }
+      end
 
-  it 'full_downcase Rus' do
-    card = Card.create(original_text: 'Дом', translated_text: 'доМ', user: user,
-                       block_id: 1)
-    expect(card.errors[:original_text]).
-      to include('Вводимые значения должны отличаться.')
-  end
+      context 'and after third review' do
+        before { 3.times { card.check_translation('house') } }
 
-  it 'create card original_text OK' do
-    card = Card.create(original_text: 'дом', translated_text: 'house',
-                       user: user, block_id: 1)
-    expect(card.original_text).to eq('дом')
-  end
+        it { expect(card.review_date).to eq(Time.zone.now + 16.days) }
+      end
 
-  it 'create card translated_text OK' do
-    card = Card.create(original_text: 'дом', translated_text: 'house',
-                       user: user, block_id: 1)
-    expect(card.translated_text).to eq('house')
-  end
+      context 'and after fifth review' do
+        before { 5.times { card.check_translation('house') } }
 
-  it 'create card errors OK' do
-    card = Card.create(original_text: 'дом', translated_text: 'house',
-                       user: user, block_id: 1)
-    expect(card.errors.any?).to be false
-  end
+        it { expect(card.review_date).to eq(Time.zone.now + 131.days) }
+      end
+    end
 
-  it 'set_review_date OK' do
-    card = Card.create(original_text: 'дом', translated_text: 'house',
-                       user: user, block_id: 1)
-    expect(card.review_date.strftime('%Y-%m-%d %H:%M')).
-      to eq(Time.zone.now.strftime('%Y-%m-%d %H:%M'))
-  end
+    context 'when wrong translation' do
+      context 'and after first review' do
+        before { card.check_translation('haus') }
 
-  it 'check_translation Eng OK' do
-    card = Card.create(original_text: 'дом', translated_text: 'house',
-                       user: user, block_id: 1)
-    check_result = card.check_translation('house')
-    expect(check_result[:state]).to be true
-  end
+        it { expect(card.review_date).to eq(Time.zone.now) }
+      end
 
-  it 'check_translation Eng NOT' do
-    card = Card.create(original_text: 'дом', translated_text: 'house',
-                       user: user, block_id: 1)
-    check_result = card.check_translation('RoR')
-    expect(check_result[:state]).to be false
-  end
+      context 'and after twice correct answer and next wrong answer' do
+        before do
+          2.times { card.check_translation('house') }
+          card.check_translation('haus')
+        end
 
-  it 'check_translation Rus OK' do
-    card = Card.create(original_text: 'house', translated_text: 'дом',
-                       user: user, block_id: 1)
-    check_result = card.check_translation('дом')
-    expect(check_result[:state]).to be true
-  end
-
-  it 'check_translation Rus NOT' do
-    card = Card.create(original_text: 'house', translated_text: 'дом',
-                       user: user, block_id: 1)
-    check_result = card.check_translation('RoR')
-    expect(check_result[:state]).to be false
-  end
-
-  it 'check_translation full_downcase Eng OK' do
-    card = Card.create(original_text: 'ДоМ', translated_text: 'hOuSe',
-                       user: user, block_id: 1)
-    check_result = card.check_translation('HousE')
-    expect(check_result[:state]).to be true
-  end
-
-  it 'check_translation full_downcase Eng NOT' do
-    card = Card.create(original_text: 'ДоМ', translated_text: 'hOuSe',
-                       user: user, block_id: 1)
-    check_result = card.check_translation('RoR')
-    expect(check_result[:state]).to be false
-  end
-
-  it 'check_translation full_downcase Rus OK' do
-    card = Card.create(original_text: 'hOuSe', translated_text: 'ДоМ',
-                       user: user, block_id: 1)
-    check_result = card.check_translation('дОм')
-    expect(check_result[:state]).to be true
-  end
-
-  it 'check_translation full_downcase Rus NOT' do
-    card = Card.create(original_text: 'hOuSe', translated_text: 'ДоМ',
-                       user: user, block_id: 1)
-    check_result = card.check_translation('RoR')
-    expect(check_result[:state]).to be false
-  end
-
-  it 'create card witout block_id' do
-    card = Card.create(original_text: 'дом', translated_text: 'house',
-                       user: user)
-    expect(card.block.title).to eq('Default')
-  end
-
-  it 'check_translation Eng OK levenshtein_distance' do
-    card = Card.create(original_text: 'дом', translated_text: 'hous',
-                       user: user, block_id: 1)
-    check_result = card.check_translation('house')
-    expect(check_result[:state]).to be true
-  end
-
-  it 'check_translation Eng OK levenshtein_distance=1' do
-    card = Card.create(original_text: 'дом', translated_text: 'hous',
-                       user: user, block_id: 1)
-    check_result = card.check_translation('house')
-    expect(check_result[:distance]).to be 1
-  end
-
-  it 'check_translation Rus OK levenshtein_distance' do
-    card = Card.create(original_text: 'house', translated_text: 'до',
-                       user: user, block_id: 1)
-    check_result = card.check_translation('дом')
-    expect(check_result[:state]).to be true
-  end
-
-  it 'check_translation Rus OK levenshtein_distance=1' do
-    card = Card.create(original_text: 'house', translated_text: 'до',
-                       user: user, block_id: 1)
-    check_result = card.check_translation('дом')
-    expect(check_result[:distance]).to be 1
-  end
-
-  it 'check_translation Eng NOT levenshtein_distance=2' do
-    card = Card.create(original_text: 'дом', translated_text: 'hou',
-                       user: user, block_id: 1)
-    check_result = card.check_translation('RoR')
-    expect(check_result[:state]).to be false
-  end
-
-  it 'check_translation Rus NOT levenshtein_distance=2' do
-    card = Card.create(original_text: 'house', translated_text: 'д',
-                       user: user, block_id: 1)
-    check_result = card.check_translation('RoR')
-    expect(check_result[:state]).to be false
+        it { expect(card.review_date).to eq(Time.zone.now + 6.days) }
+      end
+    end
   end
 end
